23b3d2cc5b86dfb234adcedaba0f6940e4a1f4b6
n2-freevas <nonodethcloud@icloud.com>
2020-01-02 00:01:37 +0900

    2019-1-1_blogkousin

---
+++ js/n2freevas_blogtop.js
@@ -0,0 +1,34 @@
+function pushArtIcon(){
+    $(".art").css("display","block");
+    $(".physical").css("display","none");
+    $(".study").css("display","none");
+    $(".other").css("display","none");
+}
+
+function pushPhysicalIcon(){
+    $(".art").css("display","none");
+    $(".physical").css("display","block");
+    $(".study").css("display","none");
+    $(".other").css("display","none");
+}
+
+function pushStudyIcon(){
+    $(".art").css("display","none");
+    $(".physical").css("display","none");
+    $(".study").css("display","block");
+    $(".other").css("display","none");
+}
+
+function pushOtherIcon(){
+    $(".art").css("display","none");
+    $(".physical").css("display","none");
+    $(".study").css("display","none");
+    $(".other").css("display","block");
+}
+
+function pushSeeallIcon(){
+    $(".art").css("display","block");
+    $(".physical").css("display","block");
+    $(".study").css("display","block");
+    $(".other").css("display","block");
+}
